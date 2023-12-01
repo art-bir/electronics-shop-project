3c4bce7f51330d8d38a33da8be1301b6709b85b6
art_bir <art.bir.1987@gmail.com>
2023-12-01 13:19:05 +0300

    Add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,14 +5,6 @@ import pytest
 from src.item import Item
 
 
-# @pytest.mark.skip(reason="no way of currently testing this")
-# @pytest.mark.parametrize("name, price, quantity, expected",
-#                          [("Кабель", 10, 5, []),
-#                           ("Мышка", 50, 5, [])])
-# def test_item_(name, price, quantity, expected):
-#     assert Item(name, price, quantity).all == []
-
-
 @pytest.mark.parametrize("name, price, quantity",
                          [("Мышка", 50, 5)])
 def test_item(name, price, quantity):
@@ -36,3 +28,22 @@ def test_apply_discount(name, price, quantity):
     item = Item(name, price, quantity)
     item.apply_discount()
     assert item.price == price * Item.pay_rate
+
+
+def test_name():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    item.name = 'СуперСмартфон'
+    # Exception: Длина наименования товара превышает 10 символов.
+    assert item.name == 'СуперСмарт'
+    assert len(item.name) <= 10
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('tests/test.csv')
+    assert len(Item.all) == 5
+
+@pytest.mark.parametrize("value, expected", [('5', 5), ('5.0', 5), ('5.5', 5)])
+def test_string_to_number(value, expected):
+    assert Item.string_to_number(value) == expected
