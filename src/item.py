7c053ce218f08ec4b2308fa3fec217b0f16ec112
art_bir <art.bir.1987@gmail.com>
2023-12-01 03:45:10 +0300

    Add functions

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,43 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if len(value) > 10:
+            value = value[:10]
+        self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls, csvfile: str) -> None:
+        """
+        Создание экземпляра класса item из файла.
+
+        :param csvfile: Путь к файлу с данными по товарам.
+        """
+        cls.all = []
+        with open(csvfile, mode='r', encoding='cp1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls(row['name'], int(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """
+        Преобразование строки в число.
+
+        :param value: Преобразованная строка.
+        :return: Преобразованное число.
+        """
+        return int(float(value))
 
     def calculate_total_price(self) -> float:
         """
